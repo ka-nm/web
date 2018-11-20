c4487fb23ed2ce4479157266d28131c083118a78
Matthew Kowalski <matthewkowalski@northwesternmutual.com>
2018-11-20 14:44:37 -0600

    wires up basic Dyanmo usage

--- api/device/index.js
+++ api/device/index.js
@@ -1,3 +1,32 @@
+const { parse } = require('url');
+const DynamoDb = require('aws-sdk/clients/dynamodb');
+
+const db = new DynamoDb({
+  apiVersion: '2012-08-10',
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECRET
+  }
+});
+
 module.exports = (req, res) => {
-  res.end('Hello, World');
+  const { query } = parse(req.url, true);
+  db.getItem({
+    TableName: 'devices',
+    Key: { deviceId: { S: query.deviceId } }
+  }, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.statusCode = 500;
+      return res.end('Unexpected Error');
+    }
+
+    if (!data.Item) {
+      res.statusCode = 404;
+      return res.end('Not Found');
+    }
+
+    res.end(JSON.stringify(data.Item));
+  })
 }
