a7a15bc0b2ce0279eae84b4605e4fde6710b0916
Mike Massie <mikemassie@northwesternmutual.com>
2018-12-12 15:29:24 -0600

    Non-working Setup Changes

--- app/src/components/Bucket.vue
+++ app/src/components/Bucket.vue
@@ -8,7 +8,42 @@
       </v-btn>
     </v-toolbar>
     <v-card-text>
-      <h3>Oh hai</h3>
+      <v-text-field
+          label="Goal Name:"
+          placeholder="College"
+          box
+          required
+          @click:append-outer="deposit"
+          :disabled="isBusy"
+        ></v-text-field>
+        <v-text-field
+          label="Goal Total:"
+          prefix="$"
+          placeholder="200.00"
+          box
+          required
+          @click:append-outer="deposit"
+          :disabled="isBusy"
+        ></v-text-field>
+        <v-text-field
+          label="Starting savings toward goal:"
+          prefix="$"
+          placeholder="10.00"
+          box
+          required
+          @click:append-outer="deposit"
+          :disabled="isBusy"
+        ></v-text-field>
+        <v-text-field
+          label="Percentage allocated from each deposit:"
+          prefix="%"
+          placeholder="60"
+          box
+          required
+          @click:append-outer="deposit"
+          :disabled="isBusy"
+        ></v-text-field>
+    </v-card-text>
     </v-card-text>
   </v-card>
 </template>
--- app/src/views/Setup.vue
+++ app/src/views/Setup.vue
@@ -1,18 +1,52 @@
 <template>
   <v-card class="elevation-12">
     <v-toolbar dark color="primary">
-      <v-toolbar-title>Setup</v-toolbar-title>
+      <v-toolbar-title>Add a Goal</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon to="/">
         <v-icon>keyboard_backspace</v-icon>
       </v-btn>
     </v-toolbar>
     <v-card-text>
-      <h3>Oh hai</h3>
+      <v-text-field
+          label="Goal Name:"
+          placeholder="College"
+          box
+          required
+          @click:append-outer="deposit"
+          :disabled="isBusy"
+        ></v-text-field>
+        <v-text-field
+          label="Goal Total:"
+          prefix="$"
+          placeholder="200.00"
+          box
+          required
+          @click:append-outer="deposit"
+          :disabled="isBusy"
+        ></v-text-field>
+        <v-text-field
+          label="Starting savings toward goal:"
+          prefix="$"
+          placeholder="10.00"
+          box
+          required
+          @click:append-outer="deposit"
+          :disabled="isBusy"
+        ></v-text-field>
+        <v-text-field
+          label="Percentage allocated from each deposit:"
+          prefix="%"
+          placeholder="60"
+          box
+          required
+          @click:append-outer="deposit"
+          :disabled="isBusy"
+        ></v-text-field>
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="primary">Submit
+      <v-btn color="primary">Save New Goal
         <v-icon right dark>cloud_upload</v-icon>
       </v-btn>
     </v-card-actions>
@@ -21,6 +55,7 @@
 
 <script>
 export default {
-  name: "setup"
+  name: "setup",
+  props:{}
 };
 </script>
