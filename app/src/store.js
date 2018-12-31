36adf100751124e99bd1f63336deeccef0a15789
Matthew Kowalski <matthewkowalski@northwesternmutual.com>
2018-12-30 19:01:29 -0600

    fixes faceplam bug

--- app/src/store.js
+++ app/src/store.js
@@ -56,7 +56,7 @@ export default new Vuex.Store({
         updatedDevice.goals.length = 0;
         device.goals.forEach(g => updatedDevice.goals.push(g));
 
-        await axios.put(`${state.baseUrl}/api/device/${device.deviceId}`, device);
+        await axios.put(`${state.baseUrl}/api/device/${device.deviceId}`, updatedDevice);
         Vue.ls.set('device', device);
         commit('setDevice', device);
         return true;
