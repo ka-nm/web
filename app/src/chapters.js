b1a1a349ed247a2e3c6ae977aab9d31041219e1c
Scott Butler <scottbutler@northwesternmutual.com>
2020-12-16 13:38:33 -0500

    added new images and text for chapter 1. Added in links section to the data structure for questions at the end of a chapter

--- app/src/chapters.js
+++ app/src/chapters.js
@@ -18,26 +18,37 @@ export default [
           released: false,
           pages: [
             {
-              image: "Intro image 1.png",
-              text: "‘Whose idea was it to move in the middle of winter?’, Penny Piggle wondered, as her breath frosted the glass of the piggy-mini van window."
+              image: "001-Kitchen Enter.png",
+              text: "The kitchen gleamed and glowed with shiny counters, and what seemed like a million drawers and cabinets."
             },
             {
-              image: "Intro image 2.png",
-              text: "They'd been packing for weeks. All Penny and her brother Rex knew was that they were going to a bigger place."
+              image: "002-RexPirate.jpeg",
+              text: "Rex loves a good mystery, and wondered what this new house had in store. He felt like a detective, or a pirate looking for buried treasure."
             },
             {
-              image: "Intro image 3.png",
-              text: "Penny and Rex’s parents couldn’t wait to show them in person. They wanted to keep everything about their new house a surprise, so they didn’t show the piglets any pictures."
+              image: "003-LookBehindDoor.jpeg",
+              text: "Rex wanted to look high and low for any new surprises. He saw a door in the corner of the room. \"Hey, Penny, look behind that door\"."
             },
             {
-              image: "Intro image 4.png",
-              text: "Penny sat with her snout against the glass, squinting to see through the falling ice and snow. ‘How did their new house look?’, she worried. 'Would she like it?', she thought."
+              image: "004-MomSearchCorner.png",
+              text: "\"Mom, will you please look for clues on that corner of the kitchen?\", said Rex. \"Sure, Rex\", Mama replied. "
             },
             {
-              image: "Intro image 5.png",
-              text: "It felt like they'd been driving forever. Finally, Mama Piggle turned around. \"Look! We're almost to our new house! Would you two like a clue?\"."
+              image: "005-DadSearchCabinets.png",
+              text: "\"I'll take the tall cabinets\", said Papa Piggle. \"I'll search the drawers\", said Rex. And they all set about their search."
             },
-          ]
+          ],
+          questions: {
+            kids: [
+              "Question 1 for the kids",
+              "Question 2 for the kids"
+            ],
+            parents: [
+              "Question 1 for the parents",
+              "Question 2 for the parents"
+            ],
+            links: "pigSettings"
+          }
         },
         {
           title: "Chapter2",
