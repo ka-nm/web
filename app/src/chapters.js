1c4e611696b33b4446b469a7f352378cfd540ff2
Scott Butler <scottbutler@northwesternmutual.com>
2020-12-23 10:31:53 -0500

    updated chapter 1 story with latest pictures and text

--- app/src/chapters.js
+++ app/src/chapters.js
@@ -22,20 +22,104 @@ export default [
               text: "The kitchen gleamed and glowed with shiny counters, and what seemed like a million drawers and cabinets."
             },
             {
-              image: "002-RexPirate.jpeg",
+              image: "002-RexPirate.jpg",
               text: "Rex loves a good mystery, and wondered what this new house had in store. He felt like a detective, or a pirate looking for buried treasure."
             },
             {
-              image: "003-LookBehindDoor.jpeg",
+              image: "003-LookBehindDoor.jpg",
               text: "Rex wanted to look high and low for any new surprises. He saw a door in the corner of the room. \"Hey, Penny, look behind that door\"."
             },
             {
               image: "004-MomSearchCorner.png",
-              text: "\"Mom, will you please look for clues on that corner of the kitchen?\", said Rex. \"Sure, Rex\", Mama replied. "
+              text: "\"Mom, will you please look for clues on that corner of the kitchen?\", said Rex. \"Sure, Rex\", Mama replied."
             },
             {
               image: "005-DadSearchCabinets.png",
               text: "\"I'll take the tall cabinets\", said Papa Piggle. \"I'll search the drawers\", said Rex. And they all set about their search."
+            },
+            {
+              image: "006 - Dad Reaching.png",
+              text: "Papa Piggle opened each cabinet and felt all the way to the back. Mama Piggle did the same in her corner."
+            },
+            {
+              image: "007 - Penny Pantry.png",
+              text: "Penny opened the door to see a pantry, full of empty shelves. She felt along each shelf for clues."
+            },
+            {
+              image: "008 - Rex Hears Digi Help.png",
+              text: "Close to one of the drawers, Rex heard a muffled voice. \"Herlp flea!\", said the voice. \"Did anybody hear that?\", Rex said."
+            },
+            {
+              image: "009 - Rex and Penny Listen.png",
+              text: "\"What?\" said Penny, running over. She hadn't found anything in the pantry. \"I swore I heard something\", said Rex."
+            },
+            {
+              image: "010 - Rex and Penny Hear Help.png",
+              text: "\"Hrlp\". This time, they both heard it. The sound was coming from the drawer. Rex tried to pull on the drawer, but it was jammed."
+            },
+            {
+              image: "011 - Rex and Penny Pull.png",
+              text: "Rex and Penny tugged together on the drawer. "
+            },
+            {
+              image: "012 - Digi Released.png",
+              text: "Suddenly it gave way. The piglets flew backwards, and they saw something flipping and turning through the air. It landed with a thud on the ground in front of them. It was . . . another piggy."
+            },
+            {
+              image: "013 - Family Stare.jpg",
+              text: "Dazed, all of the Piggles stood, staring at the green pig that landed on the floor.  Everyone was very quiet."
+            },
+            {
+              image: "014 - Digi Standing.png",
+              text: "Suddenly, lights appeared on its belly, glowing brightly. It spelled the word, \"Hi\"."
+            },
+            {
+              image: "015 - Mom Don't Touch.jpg",
+              text: "\"Don't touch it!\", Mama Piggle squealed.  \"It could be dangerous\""
+            },
+            {
+              image: "016 - Digi Homeland.jpg",
+              text: "\"It's ok\", said the the green pig. \"I promise I'm nice. My name is Digi. The last owners of the house forgot to return me to my home land, before they left. Can you help me find my way there?\""
+            },
+            {
+              image: "017 - Digi Mom Convo.jpg",
+              text: "\"Your homeland?\", Mama asked. \"Yes,\" said Digi. \"Does this have anything to do with a magic room?\" \"You know about the magic room?\", said Digi, smiling?"
+            },
+            {
+              image: "018 - Penny Response.jpg",
+              text: "\"Well, sort of\", Penny explained. \"We got a letter that said there's a secret room in this house. But, but we don't know where it is, or why it's special.\""
+            },
+            {
+              image: "019 - Digi Secret Room.jpg",
+              text: "\"Oh, there is a very special secret room in this house\", said Digi. \"One where many amazing things can happen for your whole family\"."
+            },
+            {
+              image: "020 - Rex Like What.jpg",
+              text: "\"Like what?\", said Rex?"
+            },
+            {
+              image: "021 - Digi Goal.jpg",
+              text: "\"Well\", Digi replied. \"Do you know what a goal is?\""
+            },
+            {
+              image: "022 - Penny Soccer.jpg",
+              text: "\"In soccer, it's where you kick the ball to score a point\", said Penny."
+            },
+            {
+              image: "023 - Papa and Penny.jpg",
+              text: "\"That's very right, Penny\", said Papa Piggle. \"But a goal can be something else, too.\""
+            },
+            {
+              image: "024 - Rex and Mama.jpg",
+              text: "\"Like what?\", said Rex, a second time. \"Well, said Mama Piggle, \"A goal is something important that a person wants to do or achieve. Then they make a plan to get it done, and make choices that help them get what they want\"."
+            },
+            {
+              image: "025 - Digi Exactly.jpg",
+              text: "\"Exactly!\", said DigiPiggy. \"For example, right now we all have the same goal. To find the magic room\". \"If you can help me find that magic room, I promise to tell you all I know about how to make amazing things happen for the Piggles family. Like magic!\""
+            },
+            {
+              image: "026 - Digi Joins Piggles.jpg",
+              text: "\"Will you bring me with you, as you look for my magic land?\", \"Sure!\", said Mama Piggle (who felt a lot safer about the mysterious piggy). The family picked up the piggy, and set off to meet their goal."
             },
           ],
           questions: {
